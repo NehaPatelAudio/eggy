4f30e9a1e99cf84227eef2d197a172829eee3ac4
NehaPatelAudio <61432834+NehaPatelAudio@users.noreply.github.com>
2020-10-24 15:05:25 -0400

    project initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController characterController;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(move * Time.deltaTime);
+        if (move.x !=0 || move.z !=0)
+        {
+            animator.SetBool("walk",true);
+        } 
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+    }
+}
